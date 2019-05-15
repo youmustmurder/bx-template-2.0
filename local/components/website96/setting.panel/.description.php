0c9c8de38f0dd7c0a4bc71518955106f29069ccb
Lukmanov Mikhail <lukmanof92@gmail.com>
2019-05-15 13:15:45 +0500

    feat(component) add setting.panel

---
+++ local/components/website96/setting.panel/.description.php
@@ -0,0 +1,12 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+	"NAME" => GetMessage("WEBSITE96_COLOR_PANEL_COMPONENT_NAME"),
+	"DESCRIPTION" => GetMessage("WEBSITE96_COLOR_PANEL_COMPONENT_DESC"),
+	"PATH" => array(
+	    "NAME" => 'Компоненты Website96',
+		"ID" => "website96",
+	),
+);
+?>
---
+++ local/components/website96/setting.panel/lang/ru/.description.php
@@ -0,0 +1,3 @@
+<?php
+$MESS['WEBSITE96_COLOR_PANEL_COMPONENT_NAME'] = 'Панель настройки шаблона';
+$MESS['WEBSITE96_COLOR_PANEL_COMPONENT_DESC'] = 'Изменение основных настроек шаблона';
