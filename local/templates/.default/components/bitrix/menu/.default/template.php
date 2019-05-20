8d6f352abad40ffe2d6298e453cfa3b37e56ec62
Lukmanov Mikhail <lukmanof92@gmail.com>
2019-05-20 14:10:05 +0500

    feat(components) add new components

--- local/templates/.default/components/bitrix/menu/.default/template.php
+++ local/templates/.default/components/bitrix/menu/.default/template.php
@@ -7,14 +7,12 @@ if (is_array($arResult) && !empty($arResult)) {?>
     <ul>
         <?foreach ($arResult as $i => $arItem) {?>
             <li>
-                <a <?=$arItem['SELECTED'] ? 'class="active"' : ''?>
-                   href="<?=$arItem['LINK']?>"><?=$arItem['TEXT']?></a>
+                <a <?=$arItem['SELECTED'] ? 'class="active"' : ''?> href="<?=$arItem['LINK']?>"><?=$arItem['TEXT']?></a>
                 <?if (is_array($arItem['ITEMS']) && !empty($arItem['ITEMS'])) {?>
                     <ul class="submenu">
                         <?foreach ($arItem['ITEMS'] as $item) {?>
                             <li>
-                                <a <?=$item['SELECTED'] ? 'class="active"' : ''?>
-                                   href="<?=$item['LINK']?>"><?=$item['TEXT']?></a>
+                                <a <?=$item['SELECTED'] ? 'class="active"' : ''?> href="<?=$item['LINK']?>"><?=$item['TEXT']?></a>
                             </li>
                         <?}?>
                     </ul>
