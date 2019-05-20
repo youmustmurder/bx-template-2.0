21dcab495712d279c2c5279444e2004491be46ed
Lukmanov Mikhail <lukmanof92@gmail.com>
2019-05-20 15:01:16 +0500

    fix(components) fix template name components

--- local/templates/.default/views/layouts/home.php
+++ local/templates/.default/views/layouts/home.php
@@ -15,33 +15,13 @@ $APPLICATION->IncludeFile(
 );
 
 /*sections*/
-$APPLICATION->IncludeComponent(
-	"bitrix:catalog.section.list",
-    $arCurrentSetting['SECTIONS'],
-	array(
-		"IBLOCK_TYPE" => "base",
-		"IBLOCK_ID" => "1",
-		"SECTION_ID" => $_REQUEST["SECTION_ID"],
-		"SECTION_CODE" => "",
-		"COUNT_ELEMENTS" => "Y",
-		"TOP_DEPTH" => "2",
-		"SECTION_FIELDS" => array(
-			0 => "",
-			1 => "",
-		),
-		"SECTION_USER_FIELDS" => array(
-			0 => "",
-			1 => "",
-		),
-		"FILTER_NAME" => "sectionsFilter",
-		"SECTION_URL" => "",
-		"CACHE_TYPE" => "A",
-		"CACHE_TIME" => "36000000",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_FILTER" => "N",
-		"ADD_SECTIONS_CHAIN" => "Y"
-	),
-	false
+$APPLICATION->IncludeFile(
+    "/include/" . SITE_ID . "/blocks/home-sections/" . $arCurrentSetting['SECTIONS'] . ".php",
+    array(),
+    array(
+        "SHOW_BORDER" => false,
+        "MODE" => "php"
+    )
 );
 
 /*products*/
