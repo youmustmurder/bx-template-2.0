befcaeeea986365a59312f2ea0148ba410dd3dda
Lukmanov Mikhail <lukmanof92@gmail.com>
2019-05-20 10:31:39 +0500

    feat(components) add menu .default template and add depth level

--- local/templates/.default/views/header/default/template.php
+++ local/templates/.default/views/header/default/template.php
@@ -14,9 +14,23 @@ Asset::getInstance()->addCss(GetCurDir(__DIR__) . '/style.css');
                 <div class="col-lg-7">
                     <div class="header-top__navbar">
                         <?$APPLICATION->IncludeComponent(
-                            'bitrix:menu',
-                            'default',
-                            array()
+                            "bitrix:menu",
+                            ".default",
+                            array(
+                                "COMPONENT_TEMPLATE" => ".default",
+                                "ROOT_MENU_TYPE" => "top",
+                                "MENU_CACHE_TYPE" => "N",
+                                "MENU_CACHE_TIME" => "3600",
+                                "MENU_CACHE_USE_GROUPS" => "Y",
+                                "MENU_CACHE_GET_VARS" => array(
+                                ),
+                                "MAX_LEVEL" => "2",
+                                "CHILD_MENU_TYPE" => "left",
+                                "USE_EXT" => "Y",
+                                "DELAY" => "N",
+                                "ALLOW_MULTI_SELECT" => "N"
+                            ),
+                            false
                         );?>
                     </div>
                 </div>
