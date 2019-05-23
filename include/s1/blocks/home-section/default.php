8c332b4043a3385cab172bebf23156f7a7d0e76f
slava <slava.ustinow@gmail.com>
2019-05-23 16:35:41 +0500

    feat(best categories): add default tempalte

--- include/s1/blocks/home-section/default.php
+++ include/s1/blocks/home-section/default.php
@@ -3,6 +3,9 @@
  * @author Lukmanov Mikhail <lukmanof92@gmail.com>
  */
 
+global $sectionFilter;
+$sectionFilter = array('UF_SECTION_ON_HOME' => '1');
+
 $APPLICATION->IncludeComponent(
     "bitrix:catalog.section.list",
     "home-section_default",
@@ -12,22 +15,28 @@ $APPLICATION->IncludeComponent(
         "SECTION_ID" => $_REQUEST["SECTION_ID"],
         "SECTION_CODE" => "",
         "COUNT_ELEMENTS" => "Y",
-        "TOP_DEPTH" => "2",
+        "TOP_DEPTH" => "1",
+        "SECTION_TITLE" => "",
         "SECTION_FIELDS" => array(
             0 => "",
             1 => "",
         ),
         "SECTION_USER_FIELDS" => array(
-            0 => "",
-            1 => "",
+            0 => "UF_SECTION_ON_HOME",
+            1 => "UF_SECTION_SUBTITLE",
+            2 => "UF_ANONS",
+            3 => "UF_SECTION_PERCENT",
         ),
-        "FILTER_NAME" => "sectionsFilter",
+        "FILTER_NAME" => "sectionFilter",
         "SECTION_URL" => "",
         "CACHE_TYPE" => "A",
         "CACHE_TIME" => "36000000",
         "CACHE_GROUPS" => "Y",
         "CACHE_FILTER" => "N",
-        "ADD_SECTIONS_CHAIN" => "Y"
+        "ADD_SECTIONS_CHAIN" => "N",
+        "COMPONENT_TEMPLATE" => "home-section_default",
+        "SECTION_LINK" => "",
+        "SEF_MODE" => "N"
     ),
     false
 );
--- local/templates/.default/components/bitrix/catalog.section/home-product_default/template.php
+++ local/templates/.default/components/bitrix/catalog.section/home-product_default/template.php
@@ -3,3 +3,59 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 /**
  * @author Lukmanov Mikhail <lukmanof92@gmail.com>
  */
+
+?>
+
+<section class="section-home section-products section-products__1">
+<?if($arResult['ITEMS']){?>
+<div class="section-title">
+	<div class="container">
+		<div class="row justify-content-md-between justify-content-sm-start align-items-end align-items-md-center">
+			<div class="col-auto">
+				<h2><?=$arParams['SECTION_TITLE'] ? $arParams['SECTION_TITLE'] : GetMessage('SECTION_TITLE_DEFAULT')?></h2>
+			</div>
+			<div class="col-auto">
+				<a href="/catalog/" class="btn btn-link"><?=$arParams['SECTION_LINK'] ? $arParams['SECTION_LINK'] : GetMessage('SECTION_LINK_DEFAULT')?></a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row justify-content-start">
+		<?foreach ($arResult['ITEMS'] as $key => $arProduct){
+			$this->AddEditAction($arProduct['ID'], $arProduct['EDIT_LINK'], CIBlock::GetArrayByID($arProduct["IBLOCK_ID"], "ELEMENT_EDIT"));
+			$this->AddDeleteAction($arProduct['ID'], $arProduct['DELETE_LINK'], CIBlock::GetArrayByID($arProduct["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+
+			$labelProduct = is_array($arProduct['PROPERTIES']['PRODUCT_LABEL']['VALUE_XML_ID']) ? 'label__' . $arProduct['PROPERTIES']['PRODUCT_LABEL']['VALUE_XML_ID'][0] : false;
+			?>
+			<div class="product__item col-lg-3 col-md-4 col-sm-6 col-12 <?=$arProduct['PRODUCT_LABEL']?>" id="<?=$this->GetEditAreaId($arProduct['ID']);?>">
+				<div class="product__item-image">
+					<a href="<?=$arProduct['DETAIL_PAGE_URL']?>" class="product__item-link">
+						<img lazy-images="<?=$arProduct['PREVIEW_PICTURE']['SRC']?>"
+							 alt="<?=$arProduct['PREVIEW_PICTURE']['ALT'] ? $arProduct['PREVIEW_PICTURE']['ALT'] : $arProduct['NAME']?>"
+							 title="<?=$arProduct['PREVIEW_PICTURE']['TITLE'] ? $arProduct['PREVIEW_PICTURE']['TITLE'] : $arProduct['NAME']?>">
+					</a>
+				</div>
+				<div class="product__item-desc">
+					<a href="<?=$arProduct['DETAIL_PAGE_URL']?>" class="product__item-name"><?=$arProduct['NAME']?></a>
+					<?if(is_array($arProduct['PARENT_SECTION'])){?>
+						<a href="<?=$arProduct['PARENT_SECTION']['SECTION_PAGE_URL']?>" class="product__item-category"><?=$arProduct['PARENT_SECTION']['NAME']?></a>
+					<?}?>
+				</div>
+
+				<?if(strlen($arProduct['PROPERTIES']['PRODUCT_PRICE']['VALUE']) > 0){?>
+					<div class="product__item-price">
+						<div class="product-price price__new"><?=number_format($arProduct['PROPERTIES']['PRODUCT_PRICE']['VALUE'], 0, '', ' ')?></div>
+
+						<?if(strlen($arProduct['PROPERTIES']['PRODUCT_OLD_PRICE']['VALUE']) > 0){?>
+							<div class="product-price price__old"><?=number_format($arProduct['PROPERTIES']['PRODUCT_OLD_PRICE']['VALUE'], 0, '', ' ')?></div>
+						<?}?>
+					</div>
+				<?}?>
+			</div>
+		<?}?>
+	</div>
+</div>
+<?}?>
+</section>
