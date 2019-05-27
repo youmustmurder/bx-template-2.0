222c935d7ef600eb749c199880b02e19cc1fa583
Lukmanov Mikhail <lukmanof92@gmail.com>
2019-05-27 12:02:53 +0500

    fix(home-slider_2) change names

---
+++ local/templates/.default/components/bitrix/news.list/home-slider_2/.parameters.php
@@ -0,0 +1,62 @@
+<?if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+/**
+ * @author Lukmanov Mikhail <lukmanof92@gmail.com>
+ */
+
+use Bitrix\Main\Localization\Loc;
+
+Loc::loadMessages(__FILE__);
+
+$arTemplateParameters = array(
+    "SLIDER_AUTOPLAY" => array(
+        'PARENT' => 'BASE',
+        'NAME' => Loc::getMessage('HOME_SLIDER_2_AUTOPLAY_TITLE'),
+        'TYPE' => 'CHECKBOX',
+        'DEFAULT' => 'Y'
+    ),
+    "SLIDER_ARROWS" => array(
+        'PARENT' => 'BASE',
+        'NAME' => Loc::getMessage('HOME_SLIDER_2_ARROWS_TITLE'),
+        'TYPE' => 'CHECKBOX',
+        'DEFAULT' => 'Y'
+    ),
+    "SLIDER_TIME" => array(
+        'PARENT' => 'BASE',
+        'NAME' => Loc::getMessage('HOME_SLIDER_2_AUTOPLAY_TIME_TITLE'),
+        'TYPE' => 'STRING',
+        'DEFAULT' => '3000'
+    ),
+    'NEWS_COUNT' => array(
+        'NAME' => Loc::getMessage('HOME_SLIDER_2_NEWS_COUNT_TITLE'),
+        'PARENT' => 'BASE',
+        'TYPE' => 'STRING',
+        'DEFAULT' => '5'
+    ),
+    'PREVIEW_TRUNCATE_LEN' => array('HIDDEN' => 'Y'),
+    'ACTIVE_DATE_FORMAT' => array('HIDDEN' => 'Y'),
+    'SET_TITLE' => array('HIDDEN' => 'Y'),
+    'SET_BROWSER_TITLE' => array('HIDDEN' => 'Y'),
+    'SET_META_KEYWORDS' => array('HIDDEN' => 'Y'),
+    'SET_META_DESCRIPTION' => array('HIDDEN' => 'Y'),
+    'SET_LAST_MODIFIED' => array('HIDDEN' => 'Y'),
+    'INCLUDE_IBLOCK_INTO_CHAIN' => array('HIDDEN' => 'Y'),
+    'HIDE_LINK_WHEN_NO_DETAIL' => array('HIDDEN' => 'Y'),
+    'ADD_SECTIONS_CHAIN' => array('HIDDEN' => 'Y'),
+    'DETAIL_URL' => array('HIDDEN' => 'Y'),
+    'PARENT_SECTION' => array('HIDDEN' => 'Y'),
+    'PARENT_SECTION_CODE' => array('HIDDEN' => 'Y'),
+    'INCLUDE_SUBSECTIONS' => array('HIDDEN' => 'Y'),
+    'STRICT_SECTION_CHECK' => array('HIDDEN' => 'Y'),
+    'PAGER_TEMPLATE' => array('HIDDEN' => 'Y'),
+    'DISPLAY_TOP_PAGER' => array('HIDDEN' => 'Y'),
+    'DISPLAY_BOTTOM_PAGER' => array('HIDDEN' => 'Y'),
+    'PAGER_DESC_NUMBERING_CACHE_TIME' => array('HIDDEN' => 'Y'),
+    'PAGER_TITLE' => array('HIDDEN' => 'Y'),
+    'PAGER_SHOW_ALL' => array('HIDDEN' => 'Y'),
+    'PAGER_DESC_NUMBERING' => array('HIDDEN' => 'Y'),
+    'PAGER_BASE_LINK_ENABLE' => array('HIDDEN' => 'Y'),
+    'PAGER_SHOW_ALWAYS' => array('HIDDEN' => 'Y'),
+    'SET_STATUS_404' => array('HIDDEN' => 'Y'),
+    'SHOW_404' => array('HIDDEN' => 'Y'),
+    'MESSAGE_404' => array('HIDDEN' => 'Y'),
+);
---
+++ local/templates/.default/components/bitrix/news.list/home-slider_2/lang/ru/.parameters.php
@@ -0,0 +1,5 @@
+<?
+$MESS['HOME_SLIDER_2_NEWS_COUNT_TITLE'] = 'Количество выводимых слайдов';
+$MESS['HOME_SLIDER_2_AUTOPLAY_TITLE'] = 'Включить автосмену слайдов';
+$MESS['HOME_SLIDER_2_ARROWS_TITLE'] = 'Отображать навигационные стрелки';
+$MESS['HOME_SLIDER_2_AUTOPLAY_TIME_TITLE'] = 'Время автосмены (в мс)';
