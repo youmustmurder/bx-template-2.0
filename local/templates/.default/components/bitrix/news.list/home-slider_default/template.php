9741baa623b10b2f85b170a8f33fa8df2758b1db
Lukmanov Mikhail <lukmanof92@gmail.com>
2019-06-03 12:14:05 +0500

    add slider shadow class

--- local/templates/.default/components/bitrix/news.list/home-slider_default/template.php
+++ local/templates/.default/components/bitrix/news.list/home-slider_default/template.php
@@ -14,10 +14,13 @@ if ($arResult['ITEMS']) {?>
              data-autoplay="<?=$arParams['SLIDER_AUTOPLAY'] == 'N' ? 'false' : 'true'?>"
              data-speed="<?=$arParams['SLIDER_TIME'] ?: 0?>">
             <?foreach ($arResult['ITEMS'] as $k => $arItem) {
+                dump($arItem['PROPERTIES']['SLIDE_SHADOW']['VALUE_XML_ID']);
                 $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
                 $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => Loc::getMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
                 ?>
-                <div class="slider-big__slide slider-big-slide" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+                <div class="slider-big__slide slider-big-slide
+                <?=$arItem['PROPERTIES']['SLIDE_SHADOW']['VALUE_XML_ID'] == 'Y' ? 'slider-big__shadow' : ''?>"
+                     id="<?=$this->GetEditAreaId($arItem['ID']);?>">
                     <div class="slider-big-slide__wrap">
                         <?if ($arItem['PROPERTIES']['SUBTITLE']['VALUE']) {?>
                             <div class="slider-big-slide__subtitle"><?=$arItem['PROPERTIES']['SUBTITLE']['VALUE']?></div>
