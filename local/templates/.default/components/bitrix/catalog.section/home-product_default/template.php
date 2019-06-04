f4801753976c2c5539fb0e6a92422cffaa6cbeb2
Lukmanov Mikhail <lukmanof92@gmail.com>
2019-06-04 18:14:36 +0500

    fix all home catalog products block

--- local/templates/.default/components/bitrix/catalog.section/home-product_default/template.php
+++ local/templates/.default/components/bitrix/catalog.section/home-product_default/template.php
@@ -20,53 +20,58 @@ if($arResult['ITEMS']){?>
 						<span class="link__icon"><?=GetContentSvgIcon('arrow-more');?></span>
 					</a>
 					<div class="products__grid row justify-content-start">
-						<?foreach ($arResult['ITEMS'] as $key => $arItem) {?>
-							<div class="products__item products-item col-lg-3 col-md-4 col-sm-6 col-12">
-								<?if ($arItem['PROPERTIES']['PRODUCT_LABEL']['VALUE'] || $arItem['PROPERTIES']['PRODUCT_DISCOUNT']['VALUE']) {?>
-									<ul class="products-item__labels">
-										<?if ($arItem['PROPERTIES']['PRODUCT_DISCOUNT']['VALUE']) {?>
-											<li class="label label_green">
-												<?=intval($arItem['PROPERTIES']['PRODUCT_DISCOUNT']['VALUE']) > 0 ?
-													'-' . $arItem['PROPERTIES']['PRODUCT_DISCOUNT']['VALUE'] . '%' :
-													$arItem['PROPERTIES']['PRODUCT_DISCOUNT']['VALUE']?>
-											</li>
-										<?}?>
-										<?if ($arItem['PROPERTIES']['PRODUCT_LABEL']['VALUE_XML_ID']) {?>
-											<?foreach ($arItem['PROPERTIES']['PRODUCT_LABEL']['VALUE_XML_ID'] as $k => $XML_ID) {?>
-												<li class="label <?=$XML_ID == 'NEW' ? 'label_yellow' : 'label_blue'?>">
-													<?=$arItem['PROPERTIES']['PRODUCT_LABEL']['VALUE'][$k]?></li>
-											<?}?>
-										<?}?>
-									</ul>
-								<?}?>
-								<a href="<?=$arItem['DETAIL_PAGE_URL']?>" class="products-item__image">
-									<img src="<?=$arItem['PREVIEW_PICTURE']['SRC']?>"
-										alt="<?=$arItem['PREVIEW_PICTURE']['ALT']?>"
-										title="<?=$arItem['PREVIEW_PICTURE']['TITLE']?>">
-									<button class="btn btn_mid btn_round btn_primary products-item__link">
-										<?=$arParams['MESS_BTN_DETAIL'] ?: Loc::getMessage('PRODUCTS_SECTION_READ_MORE')?>
-									</button>
-								</a>
-								<div class="products-item__desc">
-									<a href="<?=$arItem['DETAIL_PAGE_URL']?>"
-									class="products-item__name"><?=$arItem['NAME']?></a>
-									<?if ($arItem['PARENT_SECTION']) {?>
-										<a href="<?=$arItem['PARENT_SECTION']['SECTION_PAGE_URL']?>"
-										class="product__item-category"><?=$arItem['PARENT_SECTION']['NAME']?></a>
-									<?}?>
-								</div>
-								<?if ($arItem['PRICE'] || $arItem['OLD_PRICE']){?>
-									<div class="products-item__price-wrap">
-										<div class="products-item__price products-item__price_new">
-											<?=$arItem['PRICE']['VALUE']?><?=$arItem['PRICE']['CURRENCY'] == 'Y' ? ' ₽' : ''?>
-										</div>
-										<?if ($arItem['OLD_PRICE']){?>
-											<div class="products-item__price products-item__price_old">
-												<?=$arItem['OLD_PRICE']['VALUE']?><?=$arItem['OLD_PRICE']['CURRENCY'] == 'Y' ? ' ₽' : ''?>
-											</div>
-										<?}?>
-									</div>
-								<?}?>
+						<?foreach ($arResult['ITEMS'] as $key => $arItem) {
+                            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                            ?>
+							<div class="col-lg-3 col-md-4 col-sm-6 col-12" >
+                                <div class="products__item products-item" id="<?=$this->GetEditAreaId($arItem['ID']); ?>">
+                                    <?if ($arItem['PROPERTIES']['PRODUCT_LABEL']['VALUE'] || $arItem['PROPERTIES']['PRODUCT_DISCOUNT']['VALUE']) {?>
+                                        <ul class="products-item__labels">
+                                            <?if ($arItem['PROPERTIES']['PRODUCT_DISCOUNT']['VALUE']) {?>
+                                                <li class="label label_green">
+                                                    <?=intval($arItem['PROPERTIES']['PRODUCT_DISCOUNT']['VALUE']) > 0 ?
+                                                        '-' . $arItem['PROPERTIES']['PRODUCT_DISCOUNT']['VALUE'] . '%' :
+                                                        $arItem['PROPERTIES']['PRODUCT_DISCOUNT']['VALUE']?>
+                                                </li>
+                                            <?}?>
+                                            <?if ($arItem['PROPERTIES']['PRODUCT_LABEL']['VALUE_XML_ID']) {?>
+                                                <?foreach ($arItem['PROPERTIES']['PRODUCT_LABEL']['VALUE_XML_ID'] as $k => $XML_ID) {?>
+                                                    <li class="label <?=$XML_ID == 'NEW' ? 'label_yellow' : 'label_blue'?>">
+                                                        <?=$arItem['PROPERTIES']['PRODUCT_LABEL']['VALUE'][$k]?></li>
+                                                <?}?>
+                                            <?}?>
+                                        </ul>
+                                    <?}?>
+                                    <a href="<?=$arItem['DETAIL_PAGE_URL']?>" class="products-item__image">
+                                        <img src="<?=$arItem['PREVIEW_PICTURE']['SRC']?>"
+                                             alt="<?=$arItem['PREVIEW_PICTURE']['ALT']?>"
+                                             title="<?=$arItem['PREVIEW_PICTURE']['TITLE']?>">
+                                        <button class="btn btn_mid btn_round btn_primary products-item__link">
+                                            <?=$arParams['MESS_BTN_DETAIL'] ?: Loc::getMessage('PRODUCTS_SECTION_READ_MORE')?>
+                                        </button>
+                                    </a>
+                                    <div class="products-item__desc">
+                                        <a href="<?=$arItem['DETAIL_PAGE_URL']?>"
+                                           class="products-item__name"><?=$arItem['NAME']?></a>
+                                        <?if ($arItem['PARENT_SECTION']) {?>
+                                            <a href="<?=$arItem['PARENT_SECTION']['SECTION_PAGE_URL']?>"
+                                               class="product__item-category"><?=$arItem['PARENT_SECTION']['NAME']?></a>
+                                        <?}?>
+                                    </div>
+                                    <?if ($arItem['PRICE'] || $arItem['OLD_PRICE']){?>
+                                        <div class="products-item__price-wrap">
+                                            <div class="products-item__price products-item__price_new">
+                                                <?=$arItem['PRICE']['VALUE']?><?=$arItem['PRICE']['CURRENCY'] == 'Y' ? ' ₽' : ''?>
+                                            </div>
+                                            <?if ($arItem['OLD_PRICE']){?>
+                                                <div class="products-item__price products-item__price_old">
+                                                    <?=$arItem['OLD_PRICE']['VALUE']?><?=$arItem['OLD_PRICE']['CURRENCY'] == 'Y' ? ' ₽' : ''?>
+                                                </div>
+                                            <?}?>
+                                        </div>
+                                    <?}?>
+                                </div>
 							</div>
 						<?}?>
 					</div>
