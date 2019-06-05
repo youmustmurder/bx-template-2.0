da19c06496a39f51ccaf886e87ddaa6220fa2264
Lukmanov Mikhail <lukmanof92@gmail.com>
2019-06-05 12:34:47 +0500

    add .access.php

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,3 +1,27 @@
 <?php
 $arUrlRewrite=array (
+  9 => 
+  array (
+    'CONDITION' => '#^/catalog/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/catalog/index.php',
+    'SORT' => 100,
+  ),
+  6 => 
+  array (
+    'CONDITION' => '#^/stocks/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/stocks/index.php',
+    'SORT' => 100,
+  ),
+  7 => 
+  array (
+    'CONDITION' => '#^/news/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/news/index.php',
+    'SORT' => 100,
+  ),
 );
