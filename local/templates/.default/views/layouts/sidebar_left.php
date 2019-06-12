f7d839e16dc348eefb54e9f9d62dd3d58a262c86
Lukmanov Mikhail <lukmanof92@gmail.com>
2019-06-12 08:13:58 +0500

    add menu for sidebar_left and fix active element

--- local/templates/.default/views/layouts/sidebar_left.php
+++ local/templates/.default/views/layouts/sidebar_left.php
@@ -35,10 +35,25 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 				<div class="col-lg-3 col-md-4 sidebar">
 					<div class="catalog-menu sidebar__block">
                         <?$APPLICATION->IncludeComponent(
-                                'bitrix:menu',
-                                '',
-                                array()
-                        )?>
+	"bitrix:menu", 
+	".default", 
+	array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"ROOT_MENU_TYPE" => "catalog",
+		"MENU_CACHE_TYPE" => "N",
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MAX_LEVEL" => "2",
+		"CHILD_MENU_TYPE" => "left",
+		"USE_EXT" => "Y",
+		"DELAY" => "N",
+		"ALLOW_MULTI_SELECT" => "N",
+		"MAX_ITEMS" => "99"
+	),
+	false
+);?>
 					</div>
 					<div class="sidebar-news sidebar__block">
 						<div class="sidebar-news__title">Акции</div>
