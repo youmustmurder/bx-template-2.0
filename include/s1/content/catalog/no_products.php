e2bf276fc15957c3e731057fb9e43d1973d59624
Lukmanov Mikhail <lukmanof92@gmail.com>
2019-06-13 11:51:10 +0500

    fix catalog templates

---
+++ include/s1/content/catalog/no_products.php
@@ -0,0 +1,3 @@
+<a href="/catalog/" class="link link_success link_icon-right best-categories__link-all">
+    Перейти в каталог <span class="link__icon"></span>
+</a>
--- local/templates/.default/components/bitrix/catalog.section/catalog-product_1/template.php
+++ local/templates/.default/components/bitrix/catalog.section/catalog-product_1/template.php
@@ -8,19 +8,10 @@ use Bitrix\Main\Localization\Loc;
 Loc::loadMessages(__FILE__);
 
 if ($arResult['ITEMS']) {?>
-<section class="products products-grey">
+<section class="products">
 	<div class="container">
 		<div class="row">
 			<div class="col">
-               
-                <div class="products__header">
-                    <h2 class="products__title"><?=$arParams['SECTION_TITLE'] ?: Loc::getMessage('PRODUCTS_SECTION_TITLE_DEFAULT')?></h2>
-                    <a href="<?=SITE_DIR?>catalog/" class="link link_success link_icon-right products__link-all">
-                        <?=$arParams['SECTION_LINK'] ?: Loc::getMessage('PRODUCTS_SECTION_LINK_DEFAULT')?>
-                        <span class="link__icon"><?=GetContentSvgIcon('arrow-more');?></span>
-                    </a>
-                </div>
-            
 				<div class="products__grid-wrap">
 					<div class="grid grid_3column">
                         <?foreach ($arResult['ITEMS'] as $k => $arItem) {
@@ -133,4 +124,18 @@ if ($arResult['ITEMS']) {?>
 		</div>
 	</div>
 </section>
+<?} else {?>
+    <?$APPLICATION->IncludeFile(
+        "/include/" . SITE_ID . "/content/catalog/menu/top.php",
+        array(),
+        array(
+            "SHOW_BORDER" => false,
+            "MODE" => "php"
+        )
+    );?>
+<?}?>
+<?if ($arResult['DESCRIPTION']) {?>
+    <div class="text">
+        <?=$arResult['DESCRIPTION']?>
+    </div>
 <?}?>
