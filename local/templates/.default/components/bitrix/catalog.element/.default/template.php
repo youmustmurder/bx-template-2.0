31502a6ad4b3f769573436173687f6f9bb88e072
slava <slava.ustinow@gmail.com>
2019-06-13 10:37:53 +0500

    update

--- local/templates/.default/components/bitrix/catalog.element/.default/template.php
+++ local/templates/.default/components/bitrix/catalog.element/.default/template.php
@@ -2,3 +2,40 @@
 /**
  * @author Lukmanov Mikhail <lukmanof92@gmail.com>
  */
+?>
+<main>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<?$APPLICATION->IncludeFile(
+					"/include/" . SITE_ID . "/blocks/breadcrumb.php",
+					array(),
+					array(
+						"SHOW_BORDER" => false,
+						"MODE" => "php"
+					)
+				);?>
+			</div>
+		</div>
+	</div>
+	<div class="page-header">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<h1><?=$APPLICATION->GetTitle()?></h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="page">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="col">
+						<?=$arParams['CONTENT']?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
