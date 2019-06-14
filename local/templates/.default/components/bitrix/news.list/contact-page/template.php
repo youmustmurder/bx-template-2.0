2db75f678fcdb37eb1fb2a7a362064c78446fb8c
slava <slava.ustinow@gmail.com>
2019-06-14 11:27:23 +0500

    feat(contact): add draft template

--- local/templates/.default/components/bitrix/news.list/contact-page/template.php
+++ local/templates/.default/components/bitrix/news.list/contact-page/template.php
@@ -20,78 +20,72 @@ echo '</pre>';*/
 $index = 1;
 if ($arResult['ITEMS']) {
     ?>
-    
-    <div class="filials__page">
-        <div class="filials__list">
-            <div id="map_container"></div>
-            
-            <? foreach ($arResult["ITEMS"] as $arItem) {
-                $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-                $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-                
-                $Yandex = explode(",", $arItem["PROPERTIES"]["C_MAP"]["VALUE"]);
-                $Yandex_X = $Yandex[0];
-                $Yandex_Y = $Yandex[1];
-                ?>
-                
-                <div class="filial__item-data"
-                     data-index="<?= $index ?>"
-                     data-name="<?= $arItem["NAME"] ?>"
-                     data-yandex-x="<?= $Yandex_X; ?>"
-                     data-yandex-y="<?= $Yandex_Y; ?>"
-                     data-address="<?= $arItem["PROPERTIES"]["C_ADDRESS"]["VALUE"]; ?>"
-                     data-email="<?= $arItem["PROPERTIES"]["C_EMAIL"]["VALUE"]; ?>"
-                     data-phone="<?
-                     foreach ($arItem["PROPERTIES"]["C_PHONE"]["VALUE"] as $arPhone) {
-                         echo $arPhone . '; ';
-                     } ?>">
-                    
-                    <div class="filial__item-name"><?= $arItem["NAME"] ?></div>
-                    <div class="filial__item-props">
-                        <ul>
-                            <?
-                            if ($arItem["PROPERTIES"]["C_ADDRESS"]["VALUE"]) { ?>
-                                <li>
-                                    <span class="prop__name">Адрес</span>
-                                    <span class="prop__value"><?= $arItem["PROPERTIES"]["C_ADDRESS"]["VALUE"]; ?></span>
-                                </li>
-                                <?
-                            } ?>
-                            <?
-                            if ($arItem["PROPERTIES"]["C_PHONE"]["VALUE"]) { ?>
-                                <li>
-                                    <span class="prop__name">Телефон<?= count($arItem["PROPERTIES"]["C_PHONE"]["VALUE"]) > 1 ? 'ы' : '' ?></span>
-                                    <?
-                                    foreach ($arItem["PROPERTIES"]["C_PHONE"]["VALUE"] as $arPhone) { ?>
-                                        <span class="prop__value"><a
-                                                href="tel:<?= $arPhone; ?>"><?= $arPhone; ?></a></span>
-                                        <?
-                                    } ?>
-                                </li>
-                                <?
-                            } ?>
-                            <?
-                            if ($arItem["PROPERTIES"]["C_EMAIL"]["VALUE"]) { ?>
-                                <li>
-                                    <span class="prop__name">Email</span>
-                                    <span class="prop__value">
-									<a href="mailto:<?= $arItem["PROPERTIES"]["C_EMAIL"]["VALUE"]; ?>"
-                                       class="mailto__link"><?= $arItem["PROPERTIES"]["C_EMAIL"]["VALUE"]; ?></a>
-								</span>
-                                </li>
-                                <?
-                            } ?>
-                        </ul>
-                    </div>
-                </div>
-                <?
-                ++$index;
-            }
-            unset($index); ?>
-        
-        </div>
-    
-    </div>
+    <div class="col">
+		<div class="filials-page">
+			<div id="map_container"></div>
+			<ul class="branches">
+				<? foreach ($arResult["ITEMS"] as $arItem) {
+					$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+					$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+					$Yandex = explode(",", $arItem["PROPERTIES"]["C_MAP"]["VALUE"]);
+					$Yandex_X = $Yandex[0];
+					$Yandex_Y = $Yandex[1];
+					?>
+					<li class="branches__item branch"
+						data-index="<?= $index ?>"
+						data-name="<?= $arItem["NAME"] ?>"
+						data-yandex-x="<?= $Yandex_X; ?>"
+						data-yandex-y="<?= $Yandex_Y; ?>"
+						data-address="<?= $arItem["PROPERTIES"]["C_ADDRESS"]["VALUE"]; ?>"
+						data-email="<?= $arItem["PROPERTIES"]["C_EMAIL"]["VALUE"]; ?>"
+						data-phone="<?
+						foreach ($arItem["PROPERTIES"]["C_PHONE"]["VALUE"] as $arPhone) {
+							echo $arPhone . '; ';
+						} ?>">
+						<div class="branch__name"><?= $arItem["NAME"] ?></div>
+						<div class="branch__info-wrap">
+							<ul class="branch__info-list">
+								<?
+								if ($arItem["PROPERTIES"]["C_ADDRESS"]["VALUE"]) { ?>
+									<li class="branch-info-list__item branch-info">
+										<span class="branch-info__name">Адрес</span>
+										<span class="branch-info__value"><?= $arItem["PROPERTIES"]["C_ADDRESS"]["VALUE"]; ?></span>
+									</li>
+									<?
+								} ?>
+								<?
+								if ($arItem["PROPERTIES"]["C_PHONE"]["VALUE"]) { ?>
+									<li class="branch-info-list__item branch-info">
+										<span class="branch-info__name">Телефон<?= count($arItem["PROPERTIES"]["C_PHONE"]["VALUE"]) > 1 ? 'ы' : '' ?></span>
+										<?
+										foreach ($arItem["PROPERTIES"]["C_PHONE"]["VALUE"] as $arPhone) { ?>
+											<span class="branch-info__value">
+												<a href="tel:<?= $arPhone; ?>" class="branch-info__link"><?= $arPhone; ?></a>
+											</span>
+										<? } ?>
+									</li>
+									<?
+								} ?>
+								<?
+								if ($arItem["PROPERTIES"]["C_EMAIL"]["VALUE"]) { ?>
+									<li class="branch-info-list__item branch-info">
+										<span class="branch-info__name">Email</span>
+										<span class="branch-info__value">
+											<a href="mailto:<?= $arItem["PROPERTIES"]["C_EMAIL"]["VALUE"]; ?>" class="branch-info__link"><?= $arItem["PROPERTIES"]["C_EMAIL"]["VALUE"]; ?></a>
+										</span>
+									</li>
+									<?
+								} ?>
+							</ul>
+						</div>
+					</li>
+					<?
+					++$index;
+				}
+				unset($index); ?>
+			</ul>
+		</div>
+	</div>
     <script>
         $(document).ready(function () {
             if ($("#map_container").length > 0) {
