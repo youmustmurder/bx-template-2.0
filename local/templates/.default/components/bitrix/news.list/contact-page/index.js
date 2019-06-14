0187877fefccc75951f13693b5d504901910f3fa
slava <slava.ustinow@gmail.com>
2019-06-14 12:18:30 +0500

    fix(map): edit function generate map

---
+++ local/templates/.default/components/bitrix/news.list/contact-page/index.js
@@ -0,0 +1,87 @@
+window.addEventListener('load', () => {
+	const mapNode = document.querySelector('#map_container');
+	if (mapNode != null) {
+		ymaps.ready(function () {
+			var branchesNode = document.querySelectorAll('.branches__item'),
+				branches = [],
+				midX = 0,
+				midY = 0,
+				center = 0;
+
+			Array.prototype.forEach.call(branchesNode, (branch) => {
+				var x = Number(branch.getAttribure('data-yandex-x')),
+					y = Number(branch.getAttribure('data-yandex-y'));
+				branches.push({
+					x,
+					y,
+					name: branch.getAttribure('data-name'),
+					address: branch.getAttribure('data-address'),
+					email: branch.getAttribure('data-email'),
+					phone: branch.getAttribure('data-phone'),
+				});
+				midX += x;
+				midY += y;
+			});
+			center = {
+				x: midX / branches.length,
+				y: midY / branches.length,
+			};
+
+			var map = new ymaps.Map('map_container', {
+					center: [center.x, center.y],
+					zoom: 11,
+				}),
+				ClusterContent = ymaps.templateLayoutFactory.createClass('<div class="claster">$[properties.geoObjects.length]</div>'),
+				clusterIcons = [{
+					href: '/local/templates/individual/public/images/icon-geo.png',
+					size: [60, 60],
+					offset: [-30, -30],
+				}],
+				myClusterer = new ymaps.Clusterer({
+					clusterIcons,
+					clusterNumbers: [1],
+					zoomMargin: [30],
+					clusterIconContentLayout: ClusterContent
+				}),
+				myBalloonLayout = ymaps.templateLayoutFactory.createClass(
+					'<address class="address-map">' +
+					'<p><strong>$[properties.name]</strong>' + '<br />' + '</p>' +
+					'<ul class="balloon-info">' +
+					'<li><strong>Адрес:&nbsp;</strong>$[properties.address]</li>' +
+					'<li><strong>Email:&nbsp;</strong>$[properties.email]</li>' +
+					'<li><strong>Телефон:&nbsp;</strong>$[properties.phone]</li>' +
+					'</ul>' +
+					'</address>'
+				),
+				Placemark = {};
+
+			branches.forEach((branch, index) => {
+				let { x, y, name, address, email, phone } = branch;
+
+				Placemark[index] = new ymaps.Placemark([x, y], {
+					name,
+					address,
+					email,
+					phone,
+					iconContent: '<div class="marker-circ"></div>',
+				}, {
+					balloonContentLayout: myBalloonLayout,
+					balloonOffset: [5, 0],
+					balloonCloseButton: true,
+					balloonMinWidth: 450,
+					balloonMaxWidth: 450,
+					balloonMinHeught: 150,
+					balloonMaxHeught: 200,
+					iconImageHref: '/local/templates/.default/public/frontend/main/images/images/icon-geo.pn',
+					iconImageSize: [60, 60],
+					iconImageOffset: [-30, -30],
+					iconLayout: 'default#imageWithContent',
+					iconactive: '/local/templates/.default/public/frontend/main/images/images/icon-geo.png'
+				});
+				myClusterer.add(Placemark[index]);
+			});
+			map.geoObjects.add(myClusterer);
+			//map.behaviors.disable("scrollZoom");
+		});
+	}
+});
---
+++ local/templates/.default/components/bitrix/news.list/contact-page/script.js
@@ -0,0 +1,81 @@
+'use strict';
+
+window.addEventListener('load', function () {
+  var mapNode = document.querySelector('#map_container');
+
+  if (mapNode != null) {
+    ymaps.ready(function () {
+      var branchesNode = document.querySelectorAll('.branches__item'),
+          branches = [],
+          midX = 0,
+          midY = 0,
+          center = 0;
+      Array.prototype.forEach.call(branchesNode, function (branch) {
+        var x = Number(branch.getAttribure('data-yandex-x')),
+            y = Number(branch.getAttribure('data-yandex-y'));
+        branches.push({
+          x: x,
+          y: y,
+          name: branch.getAttribure('data-name'),
+          address: branch.getAttribure('data-address'),
+          email: branch.getAttribure('data-email'),
+          phone: branch.getAttribure('data-phone')
+        });
+        midX += x;
+        midY += y;
+      });
+      center = {
+        x: midX / branches.length,
+        y: midY / branches.length
+      };
+      var map = new ymaps.Map('map_container', {
+        center: [center.x, center.y],
+        zoom: 11
+      }),
+          ClusterContent = ymaps.templateLayoutFactory.createClass('<div class="claster">$[properties.geoObjects.length]</div>'),
+          clusterIcons = [{
+        href: '/local/templates/individual/public/images/icon-geo.png',
+        size: [60, 60],
+        offset: [-30, -30]
+      }],
+          myClusterer = new ymaps.Clusterer({
+        clusterIcons: clusterIcons,
+        clusterNumbers: [1],
+        zoomMargin: [30],
+        clusterIconContentLayout: ClusterContent
+      }),
+          myBalloonLayout = ymaps.templateLayoutFactory.createClass('<address class="address-map">' + '<p><strong>$[properties.name]</strong>' + '<br />' + '</p>' + '<ul class="balloon-info">' + '<li><strong>Адрес:&nbsp;</strong>$[properties.address]</li>' + '<li><strong>Email:&nbsp;</strong>$[properties.email]</li>' + '<li><strong>Телефон:&nbsp;</strong>$[properties.phone]</li>' + '</ul>' + '</address>'),
+          Placemark = {};
+      branches.forEach(function (branch, index) {
+        var x = branch.x,
+            y = branch.y,
+            name = branch.name,
+            address = branch.address,
+            email = branch.email,
+            phone = branch.phone;
+        Placemark[index] = new ymaps.Placemark([x, y], {
+          name: name,
+          address: address,
+          email: email,
+          phone: phone,
+          iconContent: '<div class="marker-circ"></div>'
+        }, {
+          balloonContentLayout: myBalloonLayout,
+          balloonOffset: [5, 0],
+          balloonCloseButton: true,
+          balloonMinWidth: 450,
+          balloonMaxWidth: 450,
+          balloonMinHeught: 150,
+          balloonMaxHeught: 200,
+          iconImageHref: '/local/templates/.default/public/frontend/main/images/images/icon-geo.pn',
+          iconImageSize: [60, 60],
+          iconImageOffset: [-30, -30],
+          iconLayout: 'default#imageWithContent',
+          iconactive: '/local/templates/.default/public/frontend/main/images/images/icon-geo.png'
+        });
+        myClusterer.add(Placemark[index]);
+      });
+      map.geoObjects.add(myClusterer); //map.behaviors.disable("scrollZoom");
+    });
+  }
+});
