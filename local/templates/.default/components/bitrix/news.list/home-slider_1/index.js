1a44af78758e77f4ea2c5049828f40abf430d72a
slava <slava.ustinow@gmail.com>
2019-06-26 14:25:44 +0500

    style: update styles (headers, sliders)

--- local/templates/.default/components/bitrix/news.list/home-slider_1/index.js
+++ local/templates/.default/components/bitrix/news.list/home-slider_1/index.js
@@ -6,14 +6,8 @@ window.addEventListener('load', () => {
 
 	const positionNav = (info) => {
 		var right = -1 * (slider.getBoundingClientRect().left - document.querySelector('.tns-slide-active .slider-big-slide__inner').getBoundingClientRect().left - 15),
-			nums = slider.querySelectorAll('.slider-big-slide')[info.displayIndex].querySelector('.slider-big-slide-numbers'),
-			top;
-		if (nums != null) {
-			top = -1 *(slider.getBoundingClientRect().top - nums.getBoundingClientRect().top) + 'px';
-		} else {
-			top = 'calc(100% - 110px)';
-		}
-		nav.style.top = top;
+			nums = slider.querySelectorAll('.slider-big-slide')[info.displayIndex].querySelector('.slider-big-slide-numbers');
+
 		nav.style.right = `${right}px`;
 	};
 
